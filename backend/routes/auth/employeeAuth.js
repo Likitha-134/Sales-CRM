a775d9cdd39ee9535fc4edde1bbe0830a14f21a5
Likitha Pidugu <likithapidugu19@gmail.com>
2026-04-20 06:38:07 +0530

    deploy fix

--- backend/routes/auth/employeeAuth.js
+++ backend/routes/auth/employeeAuth.js
@@ -2,4 +2,11 @@ const router = require("express").Router();
 const { employeeLogin } = require("../../controllers/employeeAuthController");
 
 router.post("/employee-login", employeeLogin);
+const jwt = require("jsonwebtoken");
+
+const token = jwt.sign(
+  { id: user._id, role: user.role },
+  process.env.JWT_SECRET,
+  { expiresIn: "7d" }
+);
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
 require("./jobs/resetAttendance");
+
 const employeeAuthRoutes = require("./routes/auth/employeeAuth");
+const User = require("./models/User");
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-const User = require("./models/User");
 
 app.use("/api/auth", employeeAuthRoutes);
 app.use("/api/dashboard", require("./routes/dashboardRoutes"));
@@ -19,6 +22,7 @@ app.use("/api/leads", require("./routes/leadRoutes"));
 app.use("/api/settings", require("./routes/settingsRoutes"));
 app.use("/api/attendance", require("./routes/attendance"));
 app.use("/api/activity", require("./routes/activity"));
+
 
 const createAdmin = async () => {
   const adminCount = await User.countDocuments({ role: "Admin" });
@@ -36,16 +40,22 @@ const createAdmin = async () => {
   }
 };
 
+app.get("/", (req, res) => {
+  res.send("CRM Backend is Running ");
+});
 mongoose
   .connect(process.env.MONGO_URI)
   .then(async () => {
     console.log("MongoDB Connected");
-
     await createAdmin();
-
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(" Server running on", PORT));
   })
   .catch((err) => {
     console.error("MongoDB Connection Error:", err.message);
   });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on", PORT);
+});
+  
