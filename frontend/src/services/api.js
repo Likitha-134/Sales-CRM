d1dbac34b52c302514c621be7d9dc5f2b97a340f
Likitha Pidugu <likithapidugu19@gmail.com>
2026-04-20 20:56:41 +0530

    fix get error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,27 @@
 import axios from "axios";
 
+
+const getBaseURL = () => {
+  let url = process.env.REACT_APP_API_URL || "http://localhost:5000";
+  
+
+  if (url.endsWith("/")) {
+    url = url.slice(0, -1);
+  }
+  
+  return `${url}/api`;
+};
+
 const API = axios.create({
-  baseURL:  process.env.REACT_APP_API_URL + "/api"
+  baseURL: getBaseURL(),
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
